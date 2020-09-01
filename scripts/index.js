9356792ec4418f192e210b4894c5afe57db1513f
Roman Ismagilov <roman.j123@ya.ru>
2020-09-01 11:48:03 +0300

    fix/script

--- scripts/index.js
+++ scripts/index.js
@@ -4,20 +4,36 @@ let popup = document.querySelector('.popup'); // Находим блок popup
 let openPopupButton = document.querySelector('.profile__button_type_edit'); // Находим кнопку для открытия popup
 let closePopupButton = popup.querySelector('.popup__close'); // Находим кнопку для закрытия popup
 let formElement = popup.querySelector('.popup__form'); // Находим форму в DOM
-let popupInput = popup.querySelectorAll('.popup__input'); // Находим input в DOM
+let popupInput = formElement.querySelectorAll('.popup__input'); // Находим input в DOM
 let popupSaveButton = popup.querySelector('.popup__button');
 
-openPopupButton.addEventListener('click', togglePopup); // Вешаем обработчик клика на кнопку открытия popup
-closePopupButton.addEventListener('click', togglePopup); // Вешаем обработчик клика на кнопку закрытия popup
-formElement.addEventListener('submit',formSubmitHandler);
-popupSaveButton.addEventListener('click', formSubmitHandler); // Вешаем обработчик на кнопку сохранить
+function openPopup() {
+  popup.classList.add('popup_open');
+  if(popup.classList.contains('popup_open')) {
+    popupInput[0].value = profileName.textContent;
+    popupInput[1].value = profileDesc.textContent;
+  }
+}
+function closePopup() {
+  return popup.classList.remove('popup_open')
+}
+function formSubmitHandler(evt) {
+  evt.preventDefault();
+  profileName.textContent = popupInput[0].value;
+  profileDesc.textContent = popupInput[1].value;
+  closePopup();
+}
 
+openPopupButton.addEventListener('click', openPopup); // Вешаем обработчик клика на кнопку открытия popup
+closePopupButton.addEventListener('click', closePopup); // Вешаем обработчик клика на кнопку закрытия popup
+formElement.addEventListener('submit',formSubmitHandler);
+
+/*
 // Создаём функцию которая будет принимать значения textContent
 function saveData() {
   popupInput[0].value = profileName.textContent; // Первому полю присвоили value = textContent
   popupInput[1].value = profileDesc.textContent; // Второму полю присвоили value = textContent
 }
-
 // Создаём функцию обновления данных
 function updateData() {
   profileName.textContent = popupInput[0].value;
@@ -25,8 +41,11 @@ function updateData() {
 }
 // Создаём функицю togglePopup которая будет возвращать переключение класса
 function togglePopup() {
-  saveData(); // Вызываем функцию сохранения данных
-  return popup.classList.toggle('popup_open');
+  popup.classList.toggle('popup_open');
+  //Проверяем, если popup содержит popup_open: вытягиваем данные из profileName, profileDesc
+  if(popup.classList.contains('popup_open')) {
+    saveData(); // Выполняем функцию сохранения данных
+  }
 }
 // Обработчик отправки формы
 function formSubmitHandler (evt) {
@@ -34,3 +53,4 @@ function formSubmitHandler (evt) {
   updateData(); // Вызываем функцию обновления данных
   togglePopup(); // Вызываем функцию переключения класса
 }
+*/
