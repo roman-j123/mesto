0dd5a8ab18a979f36e454c0e90903fbfd95c9856
Roman Ismagilov <roman.j123@ya.ru>
2021-01-12 18:11:36 +0300

    Card.js: Добавил alt к карточке

--- src/components/Card.js
+++ src/components/Card.js
@@ -12,13 +12,6 @@ export class Card {
       .querySelector('.elements__item')
       .cloneNode(true);
   }
-  generateCard() {
-    this._element = this._getTemplate();
-    this._setEventListeners();
-    this._element.querySelector('.elements__header').textContent = this._name;
-    this._element.querySelector('.elements__image').src = this._link;
-    return this._element;
-  }
   _setEventListeners() {
     // Удаление карточки (Находим селектор кнопки -> Вешаем событие -> Возвращаем метод _deleteCard)
     this._element.querySelector('.elements__delete').addEventListener('click', ()=> {
@@ -32,6 +25,14 @@ export class Card {
     this._element.querySelector('.elements__image').addEventListener('click', () => {
       this._handleClick(this._name, this._link);
     })
+  }
+  generateCard() {
+    this._element = this._getTemplate();
+    this._setEventListeners();
+    this._element.querySelector('.elements__header').textContent = this._name;
+    this._element.querySelector('.elements__image').src = this._link;
+    this._element.querySelector('.elements__image').alt = this._name;
+    return this._element;
   }
   // Метод удаления карточки (Возвращаем удаление разметки карточки)
   _deleteCard() {
