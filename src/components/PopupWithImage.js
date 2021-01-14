ac4ce2a068b97322cd1766af7b470cc5602fd2a4
Roman Ismagilov <roman.j123@ya.ru>
2021-01-14 16:09:25 +0300

    PopupWithImage.js исправил навзания в конструкторе

--- src/components/PopupWithImage.js
+++ src/components/PopupWithImage.js
@@ -2,13 +2,13 @@ import Popup from '../components/Popup.js';
 export default class PopupWithImage extends Popup {
   constructor(popupSelector, zoomingImageSelector, zoomingFigcaptionSelector) {
     super(popupSelector);
-    this._zoomingImageSelector = document.querySelector(zoomingImageSelector);
-    this._zoomingFigcaptionSelector = document.querySelector(zoomingFigcaptionSelector);
+    this._zoomingImage = document.querySelector(zoomingImageSelector);
+    this._zoomingFigcaption = document.querySelector(zoomingFigcaptionSelector);
   }
   open(name, link) {
-    this._zoomingImageSelector.src = link;
-    this._zoomingImageSelector.alt = name;
-    this._zoomingFigcaptionSelector.textContent = name;
+    this._zoomingImage.src = link;
+    this._zoomingImage.alt = name;
+    this._zoomingFigcaption.textContent = name;
     super.open();
   }
 }
