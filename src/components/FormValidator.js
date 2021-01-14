9dd0821d6e827f13133e8865d2e0b9fbae4889bd
Roman Ismagilov <roman.j123@ya.ru>
2021-01-15 00:37:34 +0300

    FormValidator Исправил ошибки от проверяещего

--- src/components/FormValidator.js
+++ src/components/FormValidator.js
@@ -1,22 +1,22 @@
 export class FormValidator{
   constructor (params, formSelector) {
-    this._formElement = document.querySelector(formSelector);
-    this._inputList = Array.from(this._formElement.querySelectorAll(this._inputSelector));
-    this._buttonElement = this._formElement.querySelector(this._submitButtonSelector);
     this._formSelector = formSelector;
+    this._formElement = document.querySelector(formSelector);
     this._inputSelector = params.inputSelector;
     this._inputInvaildSelector = params.inputInvaildSelector;
     this._submitButtonSelector = params.submitButtonSelector;
     this._errorClass = params.errorClass;
-    this._inputErrorClass = params.inputErrorClass;
-
+    this._inputErrorClass = params.inputErrorClass
+    this._inputList = Array.from(this._formElement.querySelectorAll(this._inputSelector));
+    this._buttonElement = this._formElement.querySelector(this._submitButtonSelector);
+    this._errorList =  this._formElement.querySelectorAll(params.inputErrorClass)
   }
   _showInputError(inputElement, errorMessage) {
     this._errorElement = this._formElement.querySelector(`#${inputElement.name}_error`);
     this._inputElement = this._formElement.querySelector(`.popup__input_type_${inputElement.name}`);
     this._errorElement.textContent = errorMessage;
     this._errorElement.classList.add(this._errorClass);
-    this._inputElement.classList.add(this._inputInvaildSelector);
+    inputElement.classList.add(this._inputInvaildSelector);
 
   }
   _hideInputError(inputElement){
@@ -24,29 +24,29 @@ export class FormValidator{
     this._inputElement.classList.remove(this._inputInvaildSelector);
     this._errorElement.textContent = "";
     this._errorElement.classList.remove(this._errorClass);
-    this._inputElement.classList.remove(this._inputInvaildSelector);
+    inputElement.classList.remove(this._inputInvaildSelector);
 
   }
-  _hasInvalidInput(inputList){
-    return inputList.some((inputElement) => {
+  _hasInvalidInput(){
+    return this._inputList.some((inputElement) => {
       return !inputElement.validity.valid;
     })
   }
-  _toggleButtonState(inputList, buttonElement){
-    if(this._hasInvalidInput(inputList)) {
-      buttonElement.setAttribute('disabled', '');
+  _toggleButtonState(){
+    if(this._hasInvalidInput()) {
+      this._buttonElement.setAttribute('disabled', '');
     } else {
-      buttonElement.removeAttribute('disabled')
+      this._buttonElement.removeAttribute('disabled');
     }
   }
   disableButton() {
-    document.querySelector(this._submitButtonSelector).setAttribute('disabled', 'disabled');
+    this._buttonElement.setAttribute('disabled', 'disabled');
   }
   clearErrors() {
-    this._formElement.querySelectorAll(this._inputSelector).forEach((element) => {
+    this._inputList.forEach((element) => {
       element.classList.remove(this._inputInvaildSelector);
     });
-    this._formElement.querySelectorAll(this._inputErrorClass).forEach((element) => {
+    this._errorList.forEach((element) => {
       element.textContent = "";
     });
   }
@@ -58,11 +58,11 @@ export class FormValidator{
     }
   }
   _setEventListeners(){
-    this._toggleButtonState(this._inputList, this._buttonElement);
-    inputList.forEach((inputElement) => {
+    this._toggleButtonState();
+    this._inputList.forEach((inputElement) => {
       inputElement.addEventListener('input', () => {
         this._checkInputValidity(inputElement);
-        this._toggleButtonState(this._inputList, this._buttonElement);
+        this._toggleButtonState();
       })
     })
   }
