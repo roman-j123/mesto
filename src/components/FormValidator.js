1faecc7cb60b3fc6071b37f2d82b412c03d515c8
Roman Ismagilov <roman.j123@ya.ru>
2021-01-15 00:00:01 +0300

    FormValidator Избавил _checkInputValidity от лишних действий. Перенес подсветку инпута в showInputError и hideInputError

--- src/components/FormValidator.js
+++ src/components/FormValidator.js
@@ -6,18 +6,24 @@ export class FormValidator{
     this._inputInvaildSelector = params.inputInvaildSelector;
     this._submitButtonSelector = params.submitButtonSelector;
     this._errorClass = params.errorClass;
+    this._inputErrorClass = params.inputErrorClass;
 
   }
   _showInputError(inputElement, errorMessage) {
     this._errorElement = this._formElement.querySelector(`#${inputElement.name}_error`);
+    this._inputElement = this._formElement.querySelector(`.popup__input_type_${inputElement.name}`);
     this._errorElement.textContent = errorMessage;
     this._errorElement.classList.add(this._errorClass);
+    this._inputElement.classList.add(this._inputInvaildSelector);
+
   }
   _hideInputError(inputElement){
     this._errorElement = this._formElement.querySelector(`#${inputElement.name}_error`);
     this._inputElement.classList.remove(this._inputInvaildSelector);
     this._errorElement.textContent = "";
     this._errorElement.classList.remove(this._errorClass);
+    this._inputElement.classList.remove(this._inputInvaildSelector);
+
   }
   _hasInvalidInput(inputList){
     return inputList.some((inputElement) => {
@@ -38,18 +44,15 @@ export class FormValidator{
     this._formElement.querySelectorAll(this._inputSelector).forEach((element) => {
       element.classList.remove(this._inputInvaildSelector);
     });
-    this._formElement.querySelectorAll(params.inputErrorClass).forEach((element) => {
+    this._formElement.querySelectorAll(this._inputErrorClass).forEach((element) => {
       element.textContent = "";
     });
   }
   _checkInputValidity(inputElement){
-    this._inputElement = this._formElement.querySelector(`.popup__input_type_${inputElement.name}`);
     if(!inputElement.validity.valid) {
       this._showInputError(inputElement, inputElement.validationMessage);
-      this._inputElement.classList.add(this._inputInvaildSelector);
     } else {
       this._hideInputError(inputElement);
-      this._inputElement.classList.remove(this._inputInvaildSelector);
     }
   }
   _setEventListeners(){
