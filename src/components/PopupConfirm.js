645bd9319e5ee718916aa5a48a24e6b0f33a3fad
Roman Ismagilov <roman.j123@ya.ru>
2021-01-22 17:41:49 +0300

    PopupConfirm удалил constructor с popupSelector

--- src/components/PopupConfirm.js
+++ src/components/PopupConfirm.js
@@ -1,8 +1,5 @@
 import Popup from './Popup.js';
 export default class PopupConfirm extends Popup {
-    constructor(popupSelector) {
-        super(popupSelector)
-    }
     setEventListeners() {
         super.setEventListeners();
         this._popup.querySelector('.popup__button').addEventListener('click', () => {
